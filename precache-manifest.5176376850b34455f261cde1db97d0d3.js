9f319fc09c324f1e54ea3d0dc11d75f1cc59d870
Sorangel Contreras <solanzola933@mail.com>
2020-11-20 17:38:33 -0600

    Updates

---
+++ precache-manifest.5176376850b34455f261cde1db97d0d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27445b4a05730bf2a3fc0ae206b538d",
+    "url": "/Gif-Expert-App/index.html"
+  },
+  {
+    "revision": "ec5d55b3f68fa2f816ca",
+    "url": "/Gif-Expert-App/static/css/main.162b39eb.chunk.css"
+  },
+  {
+    "revision": "7af6859a0b512c1f03f9",
+    "url": "/Gif-Expert-App/static/js/2.b581dadb.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Gif-Expert-App/static/js/2.b581dadb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5d55b3f68fa2f816ca",
+    "url": "/Gif-Expert-App/static/js/main.d4d35dad.chunk.js"
+  },
+  {
+    "revision": "63214ee2d578755ff87d",
+    "url": "/Gif-Expert-App/static/js/runtime-main.3a17b962.js"
+  }
+]);
